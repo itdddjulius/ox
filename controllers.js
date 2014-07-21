99c175d8a977fc017bc3570d587e6e11ad63e681
NickMro <nickmro@gmail.com>
2014-07-21 13:43:52 -0700

    added tie scenario

--- controllers.js
+++ controllers.js
@@ -18,6 +18,7 @@ ticTacToe.controller('TicTacToeController', function ($scope) {
 	$scope.players = [
 		{
 			name: "player1",
+			symbol: "X",
 			row0: 0,
 			row1: 0,
 			row2: 0,
@@ -29,6 +30,7 @@ ticTacToe.controller('TicTacToeController', function ($scope) {
 		},
 		{
 			name: "player2",
+			symbol: "O",
 			row0: 0,
 			row1: 0,
 			row2: 0,
@@ -52,23 +54,30 @@ ticTacToe.controller('TicTacToeController', function ($scope) {
 	// -------- Game Logic ---------- //
 
 	$scope.box = function(cellIndex){
-			var player = "players["+$scope.turn%2+"]";
-			eval("$scope."+player+".row"+(Math.floor(cellIndex / 3))+"++");
-			eval("$scope."+player+".column"+(cellIndex % 3)+"++");
+			var player = $scope.players[$scope.turn%2];
+			var playerString = "players["+$scope.turn%2+"]";
+			eval("$scope."+playerString+".row"+(Math.floor(cellIndex / 3))+"++");
+			eval("$scope."+playerString+".column"+(cellIndex % 3)+"++");
+
 			if (cellIndex % 4 == 0){
-				eval("$scope."+player+".diagonal0++");
+				eval("$scope."+playerString+".diagonal0++");
 			}
 			if ([2,4,6].indexOf(cellIndex) > -1 ){
-				eval("$scope."+player+".diagonal1++");
+				eval("$scope."+playerString+".diagonal1++");
 			}
 			$scope.boxes[cellIndex].isDisabled = true;
 			$scope.boxes[cellIndex].clicked = true;
-			$scope.boxes[cellIndex].player = player;
+			$scope.boxes[cellIndex].player = playerString;
+			$scope.boxes[cellIndex].symbol = player.symbol;
 
 			for (props in $scope.players[$scope.turn%2]){
 					if($scope.players[$scope.turn%2][props] == 3){
 						$scope.currentPlayer = $scope.players[$scope.turn%2];
 						$scope.gameOver = true;
+					}
+					else if($scope.turn == 8){
+						$scope.gameOver = true;
+						$scope.tie = true;
 					}
 				};
 
@@ -87,8 +96,6 @@ ticTacToe.controller('TicTacToeController', function ($scope) {
 		$scope.newPlayers = angular.copy($scope.players);
 		$scope.newBoxes = angular.copy($scope.boxes);
 	}
-
-	// ------ Animations ------ //
 	
 });
 
