3f69dd7c3a9cfb38741805075ea74386eb5097b2
NickMro <nickmro@gmail.com>
2014-07-25 00:36:18 -0700

    improved firebase features and added stats page

--- controllers.js
+++ controllers.js
@@ -2,63 +2,55 @@ var ticTacToe = angular.module('ticTacToe', ['firebase']);
 
 ticTacToe
 	
-	//Set gameboard styling directive
-
-	.directive('gameboard', function() {
-		return {
-			restrict: 'A',
-			template: '<div tile ng-repeat="b in boxes" ng-click="b.clicked || gameOver || box($index)" ng-class="{tile : b.player == null, player1 : b.player == 0, player2 : b.player == 1, clicked : b.clicked}"><div class="symbol">{{boxes[$index].symbol}}</div></div>',
-			link: function (s, e, attrs){
-				e.addClass('gameboard');
-			}
-		};
-	})
-
-	//Set tile styling directive to make sizes scaleable
-
-	.directive('tile', function() {
-
-		return {
-			restrict: 'A',
-			link: function (s, e, attrs){
-				boardWidth = Math.sqrt(s.boxes.length);
-
-				e.css({
-					width: (100 / boardWidth) - (boardWidth / 2) + "%",
-					height: (100 / boardWidth) - (boardWidth / 2) + "%",
-					margin: boardWidth * 1.3 + 'px',
-					fontSize: (18 / boardWidth) + 'em'
-				})
-			}
-		};
-	})
-
-
-	//Set tile styling directive to make sizes scaleable
-
 	.controller('TicTacToeController', function ($scope, $firebase) {
 	
-		$scope.remoteBoxes = $firebase(new Firebase("https://nickmro-tic-tac-toe.firebaseio.com/remoteBoxes"));
-	 		
+		$scope.remoteGameContainer = $firebase(new Firebase("https://nickmro-tic-tac-toe.firebaseio.com/remoteGameContainer"));
+		$scope.remoteTurn = $firebase(new Firebase("https://nickmro-tic-tac-toe.firebaseio.com/remoteTurn"));
+		var playersRef = new Firebase("https://nickmro-tic-tac-toe.firebaseio.com/remotePlayers");
 
-		// -------- Boxes and Box Data -------- //
+		// -------- View Different Screens -------- //
 
-		$scope.boxes = [
-			{id: 0, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 1, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 2, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 3, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 4, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 5, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 6, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 7, player:2, clicked:false, colorMe: false, symbol: ""},
-			{id: 8, player:2, clicked:false, colorMe: false, symbol: ""}
-		];
+		$scope.welcomeView = true;
+		$scope.gameView = false;
+		$scope.pastView = false;
+
+
+		// -------- Game Container Data -------- //
+
+		//Contains data on the board and data on the players.
+
+		$scope.board = { 
+			boxes: [
+				{id: 0, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 1, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 2, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 3, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 4, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 5, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 6, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 7, player:2, clicked:false, colorMe: false, symbol: ""},
+				{id: 8, player:2, clicked:false, colorMe: false, symbol: ""}
+			],
+			winner: ""
+		};
+
+		$scope.players = $firebase(playersRef);
+
+		$scope.gameContainer = {
+
+			board: $scope.board,
+			players: $scope.players
+
+		}
+
+		// -------- Storing Past Games -------- //
+
+		$scope.pastGamesContainer = {
+				pastGames: []
+			};
 
 		
 		// -------- Turns and Other  -------- //
-
-		$scope.begin = true;
 
 		$scope.turn = 0; //will change every turn
 
@@ -66,107 +58,110 @@ ticTacToe
 
 		$scope.tie = false;
 
-		// -------- Player Data -------- //
-
-		$scope.players = [
-			{
-				name: "",
-				symbol: "X",
-				row0: 0,
-				row1: 0,
-				row2: 0,
-				column0: 0,
-				column1: 0,
-				column2: 0,
-				diagonal0: 0,
-				diagonal1: 0
-			},
-			{
-				name: "",
-				symbol: "O",
-				row0: 0,
-				row1: 0,
-				row2: 0,
-				column0: 0,
-				column1: 0,
-				column2: 0,
-				diagonal0: 0,
-				diagonal1: 0
-			}
-		];
-
 
 
 		// In order to start the game over with a clean board and player data, 
 		// we make a copy of the first dataset.
 
-		$scope.newBoxes = angular.copy($scope.boxes);
+		$scope.newBoard = angular.copy($scope.gameContainer);
 
 
 		// -------- Enter the game -------- //
 
 		$scope.gameEnter = function(){
-			$scope.begin = false;
+			$scope.playerId = playersRef.push({ name: $scope.name, symbol: "X", row0: 0, row1: 0, row2: 0, column0: 0, column1: 0, column2: 0, diagonal0: 0, diagonal1: 0}).name();
+			console.log($scope.playerId)
+			$scope.welcomeView = false;
+			$scope.gameView = true;
 			$scope.newPlayers = angular.copy($scope.players);
+			$scope.turn = 0;
 		}
 
 		
 		// -------- Game Logic ---------- //
 
+		//Executes upon clicking any box in Tic Tac Toe board
+
 		$scope.box = function(cellIndex){
 				var cellId = cellIndex.id;
 				var playTurn = $scope.turn %2 ;
-				var player = $scope.players[playTurn];
-				var playerString = "players["+playTurn+"]";
-				eval("$scope."+playerString+".row"+(Math.floor(cellId / 3))+"++");
+				var playerName = $scope.name;
+				var player = $scope.players[$scope.playerId];
+				var playerString = "players['" + $scope.playerId + "']";
+				console.log(playerString)
+				eval("$scope."+ playerString +".row"+(Math.floor(cellId / 3))+"++");
 				eval("$scope."+playerString+".column"+(cellId % 3)+"++");
 
 				if (cellId % (4) == 0){
 					eval("$scope."+playerString+".diagonal0++");
 				}
-				if (cellId % (2) == 0 && cellId > 0 && cellId < 2){
+				if (cellId % (2) == 0 && cellId > 0 && cellId < 8){
 					eval("$scope."+playerString+".diagonal1++");
 				}
 				cellIndex.clicked = true;
 				cellIndex.player = playTurn;
 				cellIndex.symbol = player.symbol;
 
-				for (props in $scope.players[playTurn]){
-						if($scope.players[playTurn][props] == 3){
-							$scope.win(playTurn, props); //Call the win function, which is created below
+				for (props in $scope.players[$scope.playerId]){
+						if($scope.players[$scope.playerId][props] == 3){
+							$scope.winFunc(props, player); //Call the win function, which is created below
 						}
-						else if($scope.turn == (8)){
-							$scope.gameOver = true;
-							$scope.tie = true;
-						}
-					};
+				}
+				
+				if($scope.turn == 8 && $scope.gameOver == false){
+						$scope.tieFunc();
+				}
 				
 				$scope.turn++;
 		};
 
 
-		// -------- Win function -------- //
+		// -------- Win Function -------- //
 
 		//This function will run every time a player wins.
 
-		$scope.win = function(playTurn, props){
-			$scope.currentPlayer = $scope.players[playTurn];
+		$scope.winFunc = function(props, player){
+			$scope.currentPlayer = $scope.players[$scope.playerId];
 			$scope.gameOver = true;
 			$scope.winningPlay = props;
-			$scope.colorCombo(props);
+			$scope.colorCombo(props, player.symbol);
+			$scope.gameContainer.board.winner = player.name; //set a property for player winner to be viewed in stats
+			$scope.pastGamesContainer.pastGames.push($scope.gameContainer); //store past games
 		}
+
+		// -------- Tie Function -------- //
+
+		$scope.tieFunc = function(){
+			$scope.gameOver = true;
+			$scope.gameContainer.board.winner = "NA";
+			$scope.pastGamesContainer.pastGames.push($scope.gameContainer); //store past games
+			$scope.tie = true;
+		}
+
+		// -------- Add Stats Function -------- //
+
 
 
 		// -------- Start New Game -------- //
 
 		$scope.startOver = function(){
-			$scope.players = $scope.newPlayers.reverse();
-			$scope.boxes = $scope.newBoxes;
+			$scope.players = $scope.newPlayers;
+			$scope.gameContainer = $scope.newBoard;
+			console.log($scope.board);
 			$scope.gameOver = false;
 			$scope.newPlayers = angular.copy($scope.players);
-			$scope.newBoxes = angular.copy($scope.boxes);
+			$scope.newBoard = angular.copy($scope.gameContainer);
 			$scope.turn = 0;
 			$scope.tie = false;
+			$scope.pastView = false;
+			$scope.gameView = true;
+		}
+
+		// -------- View Past Games -------- //
+
+		$scope.viewPastGames = function(){
+			$scope.gameView = false;
+			$scope.pastView = true;
 		}
 
 		// -------- Color the Winning Combinations -------- //
@@ -189,18 +184,26 @@ ticTacToe
 		//Create a for loop that sets a property (colorMe) in winning combo boxes.
 		//When true, these boxes will get the class winColor, which will color the letters.
 
-		$scope.colorCombo = function(prop){
+		$scope.colorCombo = function(prop, symbol){
 			for (c in $scope.combos){
 				if ($scope.winningPlay == c){
 					$scope.winCombo = $scope.combos[c];
 					for (d in $scope.winCombo){
-						$scope.boxes[$scope.winCombo[d]].colorMe = true;
+						$scope.gameContainer.board.boxes[$scope.winCombo[d]].colorMe = symbol;
 					}
 				}
 			}
 		}
 
-		$scope.remoteBoxes.$bind($scope, "boxes");
+		$scope.remoteGameContainer.$bind($scope, "gameContainer");
+		$scope.remoteTurn.$bind($scope, "turn");
+
+		// $scope.$watch("gameContainer", function() {
+		// 	console.log("Model changed!")
+		// });
+		// $scope.$watch("players", function() {
+		// 	console.log("Model changed!")
+		// });
 	
 });
 
